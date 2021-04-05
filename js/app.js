e80f9c91d62b938ea8f1033377a177d36daf472c
Kamrul Islamm <kamrul.islamm@finlex.de>
2021-04-05 17:33:09 +0200

    update

--- js/app.js
+++ js/app.js
@@ -1,21 +1,20 @@
-let XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 let deploymentPlansDefault = [];
 
 const loadJSON = (path, success, error) => {
-    var xhr = new XMLHttpRequest();
-    xhr.onreadystatechange = function () {
-        if (xhr.readyState === XMLHttpRequest.DONE) {
-            if (xhr.status === 200) {
-                if (success)
-                    success(JSON.parse(xhr.responseText));
-            } else {
-                if (error)
-                    error(xhr);
-            }
-        }
-    };
-    xhr.open("GET", path, true);
-    xhr.send();
+  const xhr = new XMLHttpRequest();
+  xhr.onreadystatechange = () => {
+      if (xhr.readyState === XMLHttpRequest.DONE) {
+          if (xhr.status === 200) {
+              if (success)
+                  success(JSON.parse(xhr.responseText));
+          } else {
+              if (error)
+                  error(xhr);
+          }
+      }
+  };
+  xhr.open("GET", path, true);
+  xhr.send();
 };
 
 loadJSON('data.json',
@@ -30,37 +29,37 @@ loadJSON('data.json',
 );
 
 const buildDeploymentPlans = (deploymentPlansDefault) => {
-    const deploymentPlansWrapper = document.getElementById('deployment-plans-wrapper');
-    const deploymentPlans = deploymentPlansDefault.map((plan, planIndex) => {
-        const deployments = plan.deployments.map((deployment, deploymentIndex) => {
-            let affectedSoftComps = [];
-            for (const log of deployment.device.actionLog) {
-                if (log.action === 'install') {
-                    affectedSoftComps.push(log.affectedSoftwareComponent.externalId);
-                }
-            }
-            affectedSoftComps = [...new Set(affectedSoftComps)];
+  const deploymentPlansWrapper = document.getElementById('deployment-plans-wrapper');
+  const deploymentPlans = deploymentPlansDefault.map((plan, planIndex) => {
+      const deployments = plan.deployments.map((deployment, deploymentIndex) => {
+          let affectedSoftComps = [];
+          for (const log of deployment.device.actionLog) {
+              if (log.action === 'install') {
+                  affectedSoftComps.push(log.affectedSoftwareComponent.externalId);
+              }
+          }
+          affectedSoftComps = [...new Set(affectedSoftComps)];
 
-            const softwareComponents = deployment.softwareComponents.map(softwareComponent => {
-                let status = '';
-                if (affectedSoftComps.includes(softwareComponent.externalId)) {
-                    status = 'checked disabled';
-                }
+          const softwareComponents = deployment.softwareComponents.map(softwareComponent => {
+              let status = '';
+              if (affectedSoftComps.includes(softwareComponent.externalId)) {
+                  status = 'checked disabled';
+              }
 
-                return getSoftwareComponentHTML(softwareComponent, status, planIndex, deploymentIndex);
-            });
+              return getSoftwareComponentHTML(softwareComponent, status, planIndex, deploymentIndex);
+          });
 
-            return getDeploymentHTML(deployment, softwareComponents);
-        });
+          return getDeploymentHTML(deployment, softwareComponents);
+      });
 
-        return getPlanHTML(plan, deployments);
-    });
+      return getPlanHTML(plan, deployments);
+  });
 
-    deploymentPlansWrapper.insertAdjacentHTML('beforeend', deploymentPlans.join(''));
+  deploymentPlansWrapper.insertAdjacentHTML('beforeend', deploymentPlans.join(''));
 };
 
 const getSoftwareComponentHTML = (softwareComponent, status, planIndex, deploymentIndex) => {
-    return `<div class="col-6">
+  return `<div class="col-6">
       <div class="card">
           <div class="card-body">
               <h4>${softwareComponent.name}</h4>
